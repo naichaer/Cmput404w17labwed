830b5dace42cbac4df745e860d1a49bf76f6e69c
naichaer <hong8@ualberta.ca>
2017-01-11 18:03:34 -0700

    new checker

--- lab1wednesday/checkversion.py
+++ lab1wednesday/checkversion.py
@@ -4,7 +4,7 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://google.com/")
+response = requests.get("https://raw.githubusercontent.com/naichaer/cmput404w17labwed/master/lab1wednesday/checkversion.py")
 
 print response.text
 print response.status_code
